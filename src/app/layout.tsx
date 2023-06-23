8642ee97ad463ecd348d6b5f6c6ea13618084922
Ville Perälä <villevpe@users.noreply.github.com>
2023-06-23 22:03:58 +0300

    feat: proper favicon and other icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Head from "next/head";
 import "./globals.css";
 
 export const metadata = {
@@ -9,11 +10,30 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <link
-        rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡️</text></svg>"
-      />
       <html lang="en">
+        <head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="theme-color" content="#000000" />
+        </head>
         <body>{children}</body>
       </html>
     </>
