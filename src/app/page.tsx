07c8d2f1acc0f2f01f1f2d622536507190445ed5
Ville Perälä <villevpe@users.noreply.github.com>
2023-06-28 21:18:51 +0300

    fix: add cache revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { PriceData } from "./types";
 const endpoint = "https://api.porssisahko.net/v1/latest-prices.json";
 
 export default async function Home() {
-  const data = (await (await fetch(endpoint)).json()) as PriceData;
+  const data = (await (
+    await fetch(endpoint, { next: { revalidate: 60 * 5 } })
+  ).json()) as PriceData;
   return <LineChart priceData={data} />;
 }
