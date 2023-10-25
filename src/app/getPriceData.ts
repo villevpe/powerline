a48e5a993d888b1a8039c38eaa31e99d1038bf74
Ville Perälä <villevpe@users.noreply.github.com>
2023-10-25 21:47:08 +0300

    feat: force caching of the data

--- src/app/getPriceData.ts
+++ src/app/getPriceData.ts
@@ -13,22 +13,17 @@ export type FormattedPriceData = {
 }[];
 
 const formatData = ({ prices }: PriceData) => {
-  // Current time minus 2 hours
-  const start = new Date().getTime() - 1000 * 60 * 60 * 2;
-
-  return prices
-    .filter(({ startDate }) => new Date(startDate).getTime() >= start)
-    .map(({ startDate, price }, index) => ({
-      x: new Date(startDate).toISOString(),
-      y: price,
-      id: `data-${index}`,
-    }));
+  return prices.map(({ startDate, price }, index) => ({
+    x: new Date(startDate).toISOString(),
+    y: price,
+    id: `data-${index}`,
+  }));
 };
 
 export const getPriceData = async (): Promise<FormattedPriceData> => {
   const data = (await (
     await fetch("https://api.porssisahko.net/v1/latest-prices.json", {
-      next: { revalidate: 60 * 5 },
+      cache: "force-cache",
     })
   ).json()) as PriceData;
 
